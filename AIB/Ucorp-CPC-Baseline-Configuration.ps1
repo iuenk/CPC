d2178e941e13e603e550345737920b975b9126fd
Ivo Uenk <79642886+iuenk@users.noreply.github.com>
2022-07-06 08:40:36 +0200

    Update Ucorp-CPC-Baseline-Configuration.ps1

--- AIB/Ucorp-CPC-Baseline-Configuration.ps1
+++ AIB/Ucorp-CPC-Baseline-Configuration.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .Synopsis
     Customization script for Azure Image Builder
 .DESCRIPTION
@@ -52,41 +52,3 @@ Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey
 
 choco install adobereader -params '"/NoUpdates"' -y
 choco install Firefox --params "/l:nl-NL /NoDesktopShortcut /NoMaintenanceService /RemoveDistributionDir" -y
-
-# Install Microsoft 365 Apps with customization
-#Write-Log -LogOutput ("Installing Microsoft 365 Apps with customization") -Path $LogFile
-#Invoke-WebRequest -Uri 'https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=languagepack&language=nl-nl&platform=x64&source=O16LAP&version=O16GA' -OutFile 'C:\AIB\OfficeSetup.exe'
-#Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/iuenk/CPC/main/AIB/OfficeConfiguration.xml' -OutFile 'C:\AIB\OfficeConfiguration.xml'
-
-#Invoke-Expression -command 'C:\AIB\OfficeSetup.exe /configure C:\AIB\OfficeConfiguration.xml'
-#Start-Sleep -Seconds 600
-
-# Install OneDrive
-#Write-Log -LogOutput ("Installing OneDrive for all users") -Path $LogFile
-#Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/p/?LinkID=844652&clcid=0x413&culture=nl-nl&country=NL' -OutFile 'C:\AIB\OneDriveSetup.exe'
-
-#Invoke-Expression -command 'C:\AIB\OneDriveSetup.exe /allusers'
-#Start-Sleep -Seconds 60
-
-# Set Wallpaper
-Write-Log -LogOutput ("Set Wallpaper") -Path $LogFile
-$WallpaperUrl = 'https://github.com/iuenk/AVD/raw/main/AIB/Ucorp-Wallpaper.jpg'
-$WallpaperLocation = 'C:\Windows\Web\Wallpaper\Ucorp-Wallpaper.jpg'
-Invoke-WebRequest -Uri $WallpaperUrl -OutFile $WallpaperLocation
-
-# Turn off Teams startup for HKLM will also be done with User GPO
-#Write-Log -LogOutput ("Remove Teams from startup apps") -Path $LogFile
-#$Key = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run32"
-#$CustomInput = "11,00,00,00,c0,bb,ab,a4,9a,66,d7,01"
-#$hexified = $CustomInput.Split(',') | ForEach-Object { "0x$_"}
-#$AttrName = "Teams"
-#New-Item -Path $Key -Force
-#New-ItemProperty -Path $Key -Name $AttrName -Value ([byte[]]$hexified) -verbose -ErrorAction 'Stop'
-
-# Remove data but keep logging
-$var="log"
-$array= @(get-childitem $path -exclude *.$var -name)
-for ($i=0; $i -lt $array.length; $i++) {
-$removepath=join-path -path $path -childpath $array[$i]
-remove-item $removepath -Recurse
-} 
